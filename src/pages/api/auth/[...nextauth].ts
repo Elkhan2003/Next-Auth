6e40b1da8e3aa95d3d7bf11b64c2b7c926cfef44
Elcho911 <98739225+Elkhan2003@users.noreply.github.com>
2023-04-13 13:28:16 +0600

    Update [...nextauth].ts

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -4,10 +4,11 @@ import GoogleProvider from "next-auth/providers/google";
 // Initialize NextAuth
 
 export default NextAuth({
-    providers: [
-        GoogleProvider({
-            clientId: "417722966225-rf8a10e9oj9dh3q9dehmovfhv1jqhf19.apps.googleusercontent.com",
-            clientSecret: "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"
-        })
-    ]
-})
+	providers: [
+		GoogleProvider({
+			clientId:
+				"417722966225-rf8a10e9oj9dh3q9dehmovfhv1jqhf19.apps.googleusercontent.com",
+			clientSecret: "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"
+		})
+	]
+});
