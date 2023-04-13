cf7319312540a553ce4bc0b97b63248f32d96494
Elcho911 <98739225+Elkhan2003@users.noreply.github.com>
2023-04-13 12:05:26 +0600

    Update Login.tsx

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -5,13 +5,13 @@ import { UserCard } from "./userCard";
 export default function Login() {
 	const { data: session, status } = useSession();
 
-	// redirect to sign-in page if user is not authenticated
 	useEffect(() => {
 		if (status === "loading") return; // do nothing while loading
-		if (!session) signIn();
+		if (!session) {
+			signIn();
+		}
 	}, [status, session]);
 
-	// if the user exists -> show a Sign Out button and their information
 	if (session) {
 		return (
 			<>
