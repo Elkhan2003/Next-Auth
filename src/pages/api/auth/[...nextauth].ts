0a4649ba7829c6a8087d16d561b760ca330ec99b
Elcho911 <98739225+Elkhan2003@users.noreply.github.com>
2023-04-13 14:59:37 +0600

    Update [...nextauth].ts

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -5,16 +5,16 @@ import GoogleProvider from "next-auth/providers/google";
 
 export default NextAuth({
 	providers: [
-		GoogleProvider({
-			clientId:
-				"417722966225-rf8a10e9oj9dh3q9dehmovfhv1jqhf19.apps.googleusercontent.com",
-			clientSecret: "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"
-		})
-
 		// GoogleProvider({
 		// 	clientId:
-		// 		"417722966225-p2ai9kjph0vhpoh3qtbq73f2tvubi18j.apps.googleusercontent.com",
-		// 	clientSecret: "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"
+		// 		"417722966225-rf8a10e9oj9dh3q9dehmovfhv1jqhf19.apps.googleusercontent.com",
+		// 	clientSecret: "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"
 		// })
+
+		GoogleProvider({
+			clientId:
+				"417722966225-p2ai9kjph0vhpoh3qtbq73f2tvubi18j.apps.googleusercontent.com",
+			clientSecret: "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"
+		})
 	]
 });
