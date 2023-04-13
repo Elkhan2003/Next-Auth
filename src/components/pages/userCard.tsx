1ea02ca36ef4a3ab67947fd7852bba51a398cb55
Elcho911 <98739225+Elkhan2003@users.noreply.github.com>
2023-04-13 13:05:59 +0600

    Update userCard.tsx

--- src/components/pages/userCard.tsx
+++ src/components/pages/userCard.tsx
@@ -2,7 +2,7 @@ import React, { FC } from "react";
 import { DefaultSession } from "next-auth";
 
 interface UserCardProps {
-	user?: DefaultSession["user"];
+	user: DefaultSession["user"];
 }
 
 const UserCard: FC<UserCardProps> = ({ user }) => {
@@ -11,7 +11,6 @@ const UserCard: FC<UserCardProps> = ({ user }) => {
 			<div className="card-body">
 				<p>Current Logged In User</p>
 				<h5 className="card-title">{user?.name}</h5>
-				<img src={user?.image} alt="user" />
 				<p className="card-text">{user?.email}</p>
 			</div>
 		</div>
